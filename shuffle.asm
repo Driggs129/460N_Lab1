536b1052f96fb776d968b67f813913887aa3fe94
Douglas Riggs <DouglasRiggs@wireless-10-145-224-100.public.utexas.edu>
2019-02-10 17:08:19 -0600

    wrong version

--- shuffle.asm
+++ shuffle.asm
@@ -1,7 +1,7 @@
-.ORIG 0x3000
+.ORIG x3000
        LEA R0, Y
        AND R6, R6, #0
-       ADD R6, R6, 0x04
+       ADD R6, R6, x04
 Loop   LDB R1, R0, #0
        LDB R2, R0, #1
        STB R1, R0, #1
@@ -12,5 +12,5 @@ Loop   LDB R1, R0, #0
        HALT
        
 
-Y .FILL  0x3050
+Y .FILL  x3050
 .END
